e59b0562a42dc3e7cf099624d6003284e20ba0ce
Gabriel Ferreira Sasdelli <sasdelle69@gmail.com>
2022-08-02 19:18:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int NumeroChamada = 0;
+            String Nome_Aluno = "Gabriel Ferreira Sasdelli ";
+
+            NumeroChamada = 1;
+            Console.WriteLine(NumeroChamada);
+            Console.WriteLine(Nome_Aluno);
+
+            Console.WriteLine(args[0].ToString());
+            Console.WriteLine(args[1].ToString());
+            Console.WriteLine(args[2].ToString());
+            Console.WriteLine(args[3].ToString());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
